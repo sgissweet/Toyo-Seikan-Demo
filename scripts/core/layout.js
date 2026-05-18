01e5d0d5a2b32100a2c7c68b5dd14d487a6ffe5f
Ariya Angsuwatana <10436ariya@gmail.com>
2026-05-18 12:55:47 +0700

    edit to use navbar footer

--- scripts/core/layout.js
+++ scripts/core/layout.js
@@ -2,38 +2,28 @@ export function getPage() {
   return document.body.dataset.page;
 }
 
-function loadComponent(selector, file, callback) {
-  fetch(file)
-    .then(res => res.text())
-    .then(data => {
-      const el = document.querySelector(selector);
-      if (!el) return;
 
-      el.innerHTML = data;
-
-      if (callback) callback(el);
-    });
-}
 
 const isHome = () => getPage() === "home";
 
-// Navbar
-loadComponent("#navbar-placeholder", "../components/navbar.html", (el) => {
-  if (isHome()) {
-    const icon = el.querySelector(".navbar-icon");
-    if (icon) icon.src = "../assets/icons/home2.png";
-  }
-  const header = document.querySelector(".site-header");
+document.addEventListener("DOMContentLoaded", () => {
 
-    if (!header) return;
+  fetch("../components/navbar.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("navbar").innerHTML = html;
 
-    
+      renderUser();
+      renderNav();
+    });
+
+  fetch("../components/footer.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("footer").innerHTML = html;
+
+      renderFooter();
+    });
+
 });
-
-// Footer
-const footerFile = isHome()
-  ? "../components/footer.html"
-  : "../components/footer2.html";
-
-loadComponent("#footer", footerFile);
 
