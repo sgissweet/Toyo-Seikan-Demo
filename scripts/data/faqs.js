5780bd838e6aaeebe7c3a6ad8239260c665f7e24
Ariya Angsuwatana <10436ariya@gmail.com>
2026-05-18 10:50:30 +0700

    add index page

---
+++ scripts/data/faqs.js
@@ -0,0 +1,29 @@
+
+export const faqData = [
+  {
+    id: "faq1",
+    question: "1. การเรียน/สอบ ผ่านระบบ E-Learning",
+    answer: `
+      <ol>
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</li>
+        <li>The standard chunk of Lorem Ipsum used since the 1500s...</li>
+      </ol>
+    `,
+    show: true
+  },
+  {
+    id: "faq2",
+    question: "2. ลืมรหัสผ่าน",
+    answer: `เนื้อหาคำตอบ...`
+  },
+  {
+    id: "faq3",
+    question: "3. วิธีแก้ปัญหาการใช้งาน",
+    answer: `เนื้อหาคำตอบ...`
+  },
+  {
+    id: "faq4",
+    question: "4. สามารถใช้งานภาษาอะไรได้บ้าง",
+    answer: `เนื้อหาคำตอบ...`
+  }
+];
