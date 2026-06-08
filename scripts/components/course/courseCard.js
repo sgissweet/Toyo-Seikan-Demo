0884083e680c49e56e04606f7c2ab491d6969a1b
Ariya Angsuwatana <10436ariya@gmail.com>
2026-06-08 13:58:14 +0700

    link index such as card button to another page

--- scripts/components/course/courseCard.js
+++ scripts/components/course/courseCard.js
@@ -12,35 +12,37 @@ export function createCourseCard(course, options = {}) {
 
     return `
         <div class="col-md-4">
-            <div class="card course-card mb-4">
-                <img src="${course.image}" class="card-img-top">
-                <div class="card-body">
-                    <small class="text-muted d-flex align-items-center">
-                        <img src="../assets/icons/type_couse.png" class="type-icon">
-                        ${course.type}
-                    </small>
-                    <h6 class="fw text-truncate">${course.title}</h6>
-
-                    <!-- Footer (with divider) -->
-<div class="course-footer">
+            <a href="../pages/Coursecontent.html" class="course-card-link">
+                <div class="card course-card mb-4">
+                    <img src="${course.image}" class="card-img-top">
+                    <div class="card-body">
                         <small class="text-muted d-flex align-items-center">
-                            <img src="../assets/icons/time.png" class="type-icon">
-                            ${formatDuration(course.duration)}
-                            ${
-                                showButton
-                                ? `
-                                    <button class="btn btn-primary btn-sm px-3 ms-auto">
-                                        เข้าเรียน
-                                    </button>
-                                `
-                                : ""
-                            }
+                            <img src="../assets/icons/type_couse.png" class="type-icon">
+                            ${course.type}
                         </small>
-                    </div>
+                        <h6 class="fw text-truncate">${course.title}</h6>
+
+                        <!-- Footer (with divider) -->
+                        <div class="course-footer">
+                            <small class="text-muted d-flex align-items-center">
+                                <img src="../assets/icons/time.png" class="type-icon">
+                                ${formatDuration(course.duration)}
+                                ${
+                                    showButton
+                                    ? `
+                                        <button class="btn btn-primary btn-sm px-3 ms-auto">
+                                            เข้าเรียน
+                                        </button>
+                                    `
+                                    : ""
+                                }
+                            </small>
+                        </div>
 
             
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     `;
 }
--- scripts/components/newsCard.js
+++ scripts/components/newsCard.js
@@ -1,14 +1,19 @@
 export function createNewsCard(news) {
     return `
         <div class="news-zone">
-            <div class="card news-card mb-4">
-                <img src="${news.image}" class="news-card-img" alt="${news.title}">
-                <div class="p-2">
-                    <p class="text-truncate">${news.title}</p>
-                    <small class="text-muted"><i class="bi bi-calendar me-2"></i>${news.date}</small>
-                    <a href="#" class="d-block text-end mt-2">อ่านเพิ่มเติม...</a>
+            <a href="../pages/news-detail.html" class="news-card-link">
+      
+                <div class="card news-card mb-4">
+                    <img src="${news.image}" class="news-card-img" alt="${news.title}">
+                    <div class="p-2">
+                        <p class="text-truncate">${news.title}</p>
+                        <small class="text-muted"><i class="bi bi-calendar me-2"></i>${news.date}</small>
+                        <div class="text-end mt-2">
+                            <span class="read-more">อ่านเพิ่มเติม...</span>
+                        </div>                    
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     `;
 }
